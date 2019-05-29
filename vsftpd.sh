bc0acacac5284620a2e3ae3fb961e87ea2e4e91c
siyuwuxin <siyuwuxin@gmail.com>
2019-05-29 23:43:13 +0800

    minor fix

--- vsftpd.sh
+++ vsftpd.sh
@@ -16,13 +16,15 @@ if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
   export PASV_ADDRESS=$(curl -s -4 --connect-timeout 5 --max-time 10 ifconfig.co)
 fi
 
-echo -e "\n## passive mode port address" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=$PASV_MAX_PORT" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=$PASV_MIN_PORT" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf
+if [ -z "$(grep "pasv_max_port=" /etc/vsftpd/vsftpd.conf)" ]; then
+  echo -e "\n## passive mode port address" >> /etc/vsftpd/vsftpd.conf
+  echo "pasv_max_port=$PASV_MAX_PORT" >> /etc/vsftpd/vsftpd.conf
+  echo "pasv_min_port=$PASV_MIN_PORT" >> /etc/vsftpd/vsftpd.conf
+  echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf
+fi
 
 # Execute add ftp user script
-if [ -n "$AUTO_ADD_USER" ]; then
+if [ -n "$AUTO_ADD_USER" ] && [ ! -s "/etc/vsftpd/virtual_users" ]; then
   if [ -s "/etc/vsftpd/vsftpd-add-ftp-user.sh" ]; then
     chmod +x /etc/vsftpd/vsftpd-add-ftp-user.sh
     /etc/vsftpd/vsftpd-add-ftp-user.sh
